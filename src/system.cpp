d9ba460a338f3c027713832ccd6055a96be89e9e
Vinicius Sa <viniciusjavs@protonmail.com>
2020-04-04 18:25:31 -0300

    Show system uptime
    It reads the system uptime from the first value of /proc/uptime.

--- src/system.cpp
+++ src/system.cpp
@@ -38,5 +38,5 @@ int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 // Returns the total number of processes on the system.
 int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
-// TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+// Returns the number of seconds since the system started running.
+long System::UpTime() { return LinuxParser::UpTime(); }
