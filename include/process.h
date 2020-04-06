c7d5c3bf02b94fd7605dba9a94f8c36a0d76d534
Vinicius Sa <viniciusjavs@protonmail.com>
2020-04-06 17:57:42 -0300

    Show processes username
    Steps:
    
     - Find the UID associated with the process: /proc/[PID]/status
    
     - Find the user corresponding to that UID: /etc/passwd

--- include/process.h
+++ include/process.h
@@ -10,7 +10,7 @@ class Process {
  public:
   Process(int pid);
   int Pid() const;
-  std::string User();                      // TODO: See src/process.cpp
+  std::string User();
   std::string Command();                   // TODO: See src/process.cpp
   float CpuUtilization();                  // TODO: See src/process.cpp
   std::string Ram();                       // TODO: See src/process.cpp
