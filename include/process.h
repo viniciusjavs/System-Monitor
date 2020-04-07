2559e30d9ec3cff93d1962ef3739820fe98feb0f
Vinicius Sa <viniciusjavs@protonmail.com>
2020-04-07 02:40:57 -0300

    Show processes memory utilization
    It reads the information from /proc/[pid]/status. Key of "VmSize".

--- include/process.h
+++ include/process.h
@@ -13,7 +13,7 @@ class Process {
   std::string User();
   std::string Command();                   // TODO: See src/process.cpp
   float CpuUtilization() const;
-  std::string Ram();                       // TODO: See src/process.cpp
+  std::string Ram();
   long UpTime();
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
 
