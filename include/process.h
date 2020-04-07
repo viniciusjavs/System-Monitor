0fb6a2184269030b2477e5ba950f6f31f7c9697c
Vinicius Sa <viniciusjavs@protonmail.com>
2020-04-07 02:45:44 -0300

    Show processes uptime
    It reads the period of the process in "clock ticks" from
    /proc/[pid]/stat (#22 position) and divide that value by
    "sysconf(_SC_CLK_TCK)" resulting in a time in seconds.

--- include/process.h
+++ include/process.h
@@ -14,7 +14,7 @@ class Process {
   std::string Command();                   // TODO: See src/process.cpp
   float CpuUtilization();                  // TODO: See src/process.cpp
   std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
+  long UpTime();
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
 
  private:
